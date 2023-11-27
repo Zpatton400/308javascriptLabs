2f1113537e7decc1a15ea419a2ee2845626fa4ff
Zena Patton <147436544+Zpatton400@users.noreply.github.com>
2023-11-27 18:20:37 -0500

    Part 1:  Fizz Buzz of ALAB 308.3.1: Practical Loops
    Objectives:
    Use for loops to iterate a specific number of times.
    Use for of loops to iterate through iterable data-like strings.
    Use while loops to iterate based on a condition.
    Use the break and continue statements to control loop flow.

---
+++ #1_ALAB30831Practical Loops.js
@@ -0,0 +1,45 @@
+// Loop Activity #1: Fizz Buzz
+
+// Using a for loop to iterate from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  // Check if the number is divisible by both 3 and 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  }
+  // Check if the number is divisible by 3
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  // Check if the number is divisible by 5
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+  // If the number is not divisible by either 3 or 5, log the number
+  else {
+    console.log(i);
+  }
+}
+// Loop Activity #1: Fizz Buzz
+
+// Using a for loop to iterate from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  // Check if the number is divisible by both 3 and 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  }
+  // Check if the number is divisible by 3
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  // Check if the number is divisible by 5
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+  // If the number is not divisible by either 3 or 5, log the number
+  else {
+    console.log(i);
+  }
+}
+
+//I use a "for loop" to iterate from 1 to 100 (inclusive).I use if statements to check the conditions in the given problem.The % operator is used to check if a number is divisible by another number.
+//Ohter possible approach:  Using "while" loop in the same way.
